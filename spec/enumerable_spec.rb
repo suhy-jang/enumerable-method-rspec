09faa59f6c00506072f62f3d92bb1064718198b3
shjang7 <lucy.sh.jang@gmail.com>
2019-06-05 03:23:07 +0900

    Add more details in test file

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,64 +1,135 @@
-require './lib/enumerable'
+require './enumerable'
 
 RSpec.describe Enumerable do
-  let(:sample_arr) { ["apple", "orange", "banana", "pear"]}
-  describe "#my_each" do
-    it "returns the array" do
-      arr = [1,2,3,4]
-      expect(arr.my_each{|p| p}).to eql(arr.each{|p| p})
+  let(:enum) {
+    obj = Object.new
+
+    def obj.each_arg(a, b=:b, *rest)
+      yield a
+      yield b
+      yield rest
+      :method_returned
     end
 
-    it "returns the enumerator of the receiver" do
-      arr = [1,2,3,4]
-      expect(arr.my_each).to be_an Enumerator
-      expect(arr.my_each.to_a).to eql(arr.each.to_a)
+    enum = obj.to_enum :each_arg, :a, :x
+  }
+
+  describe "#my_each" do
+    range = (1..4)
+    array = [:a, :x, []]
+    it "returns the enumerator of the receiver - enum" do
+      expect(enum.my_each).to be_an Enumerator
+    end
+
+    it "returns the enumerator of the receiver - range" do
+      expect(range.my_each).to be_an Enumerator
+    end
+
+    it "returns the enumerator of the receiver - array" do
+      expect(array.my_each).to be_an Enumerator
+    end
+
+    it "returns itself - enum" do
+      expect(enum.my_each).to eql(enum.each)
+    end
+
+    it "returns itself - range" do
+      expect(range.my_each{|p| p}).to eql(range.each{|p| p})
+    end
+
+    it "returns itself - array" do
+      expect(array.my_each{|p| p}).to eql(array.each{|p| p})
+    end
+
+    it "returns ':method_returned'" do
+     expect(enum.my_each { |elm| elm }).to eql(enum.each { |elm| elm })
+    end
+  end
+
+  describe "#my_each_with_index" do
+    animals = %w(cat dog wombat)
+    it "returns an enumerator if block with two arguments are given" do
+      hash = Hash.new
+      expect(animals.my_each_with_index{|item,index| hash[item] = index})
+      .to eql(animals.each_with_index{|item,index| hash[item] = index})
+    end
+
+    it "returns an enumerator if no block is given" do
+      expect(animals.my_each_with_index).to be_an Enumerator
     end
   end
 
   describe "#my_select" do
-    it "returns a new array with all the numbers what is smaller than 5 of the receiver." do
-      arr = [2,4,6,8]
-      expect(arr.my_select{|p| p < 5 }).to eql(arr.select{|p| p < 5 })
+    range = (1..10)
+    array = [1,2,3,4,5]
+    array_sym = [:foo, :bar]
+
+    it "returns an array containing 3 divisable elements only - range input" do
+      expect(range.my_select{|p| p % 3 == 0 }).to eql(range.select{|p| p % 3 == 0 })
     end
 
-    it "returns a new array with all the numbers is even" do
-      arr = [1,2,3,4,5]
-      expect(arr.my_select { |p| p.even? }).to eql(arr.select { |p| p.even? } )
+    it "returns an array containing even numbers only - array input" do
+      expect(array.my_select { |p| p.even? }).to eql(array.select { |p| p.even? } )
+    end
+
+    it "returns an array containing specific symbols only - symbol array input" do
+      expect(array_sym.my_select { |p| p == :foo })
+      .to eql(array_sym.select { |p| p == :foo } )
+    end
+
+    it "returns the enumerator of the receiver - Enum" do
+      expect(enum.my_select).to be_an Enumerator
+    end
+
+    it "returns the enumerator of the receiver - Range" do
+      expect(range.my_select).to be_an Enumerator
+    end
+
+    it "returns the enumerator of the receiver - Array" do
+      expect(array.my_select).to be_an Enumerator
     end
   end
 
   describe "#my_all?" do
-    it "returns true or false with all the numbers is even" do
-      arr = [2,4,6,8,12]
-      expect(arr.my_all? { |p| p.even? }).to eql(arr.all? { |p| p.even? })
+    it "returns true or false with every words are longer or equal than 4" do
+      animals = %w[ant bear cat]
+      expect(animals.my_all? { |word| word.length >= 4 })
+      .to eql(animals.all? { |word| word.length >= 4 })
     end
+
+    # it "returns true or false with all elements is between [0-9a-zA-Z_]" do
+    #   expect(animals.my_all?(/w/)).to eql(animals.all?(/w/))
+    # end
 
     it "returns true or false with all the numbers is Numeric" do
       arr = [1, 2i, 3.14]
-      expect(arr.my_all?(Numeric)).to eql(arr.all?(Numeric))
+      expect(arr.my_all?(Numeric)).to eql(arr.all?(Numeric)) # true
     end
 
     it "returns true or false with all the numbers is Numeric" do
       arr = [nil, true, 99]
-      expect(arr.my_all?).to eql(arr.all?)
+      expect(arr.my_all?).to eql(arr.all?) # false
     end
 
     it "returns true or false with all the numbers is Numeric" do
       arr = []
-      expect(arr.my_all?).to eql(arr.all?)
+      expect(arr.my_all?).to eql(arr.all?) # true
     end
   end
 
   describe "#my_any?" do
-    arr_make = %w[ant bear cat]
-    it "returns true or false with any the words is longer or equal than 4 in #{arr_make}" do
-      expect(arr_make.my_any? {|word| word.length >= 4})
-      .to eql(arr_make.any? {|word| word.length >= 3} )
-    end
-    arr = [nil, true, 99]
-    it "returns true or false with any input is not [0-9a-zA-Z_] in #{arr}" do
-      expect(arr.my_any?(/w/)).to eql(arr.any?(/w/))
-    end
+    # it "returns true or false with every words have tap" do
+    #   expect(animals.my_all? (/t/)).to eql(animals.all? (/t/))
+    # end
+    # animals = %w[ant bear cat]
+    # it "returns true or false with any words is longer or equal than 4 in #{animals}" do
+    #   expect(animals.my_any? {|word| word.length >= 4})
+    #   .to eql(animals.any? {|word| word.length >= 4} )
+    # end
+    # arr = [nil, true, 99]
+    # it "returns true or false with any input is not [0-9a-zA-Z_] in #{arr}" do
+    #   expect(arr.my_any?(/w/)).to eql(arr.any?(/w/))
+    # end
     arr = []
     it "returns true or false with any input is in #{arr}" do
       expect(arr.my_any?).to eql(arr.any?)
